1f6059420733bef0136928d46d0156c940eedf5c
eleonard1224 <45740519+eleonard1224@users.noreply.github.com>
2018-12-24 15:06:59 -0500

    Delete solver.h

--- solver.h
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
-
-int slu(double **L, double **U, double **A, int n);
-int slv(double **L, double **U, double **A, double *x, double *b, int n);
